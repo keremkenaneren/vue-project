3fa3c80aa182175ba73f28b3fdf7147457523bbf
Kerem Kenan Eren <114931693+ErenKerem82@users.noreply.github.com>
2023-05-20 12:03:55 +0300

    design

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,50 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div class="container">
+    <h1>Insider</h1>
+     <input type="email" name="email" class="email">
+     <input type="password" name="password">
+     <button class="save">SAVE</button>
   </div>
 </template>
 
+<script>
+
+export default {
+  
+}
+</script>
+
 <style>
-#app {
+.container {
+  display: grid;
+  margin-inline: 500px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+  text-align: left;
+  color: #000000;
+  margin-top: 60px;
+  }
 
-nav {
-  padding: 30px;
-}
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+input { 
+  background: rgb(187, 180, 181);
+  margin-bottom: 5%;
+  width: 300px;
+  height: 20px;
+  border: whitesmoke 1px solid;
+  border-radius: 5px;
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.save {
+  font-weight: 900;
+  width: 100px;
+  height: 30px;
+  color: white;
+  background: rgb(0, 0, 0);
+  border: whitesmoke 1px solid;
+  border-radius: 20px;
+}
+.save:hover {
+  background: rgb(68, 68, 68);
 }
 </style>
